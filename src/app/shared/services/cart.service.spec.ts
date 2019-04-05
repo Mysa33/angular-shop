a9f345b20dcbd931efd5aa14a801b543310ba692
Mysa33 <tpdrn.france@gmail.com>
2019-04-05 13:44:52 +0200

    Cart service

--- src/app/shared/services/cart.service.spec.ts
+++ src/app/shared/services/cart.service.spec.ts
@@ -26,4 +26,40 @@ describe('CartService', () => {
     expect(service).toBeTruthy();
   }));
 
+  it('should doTotal() to be defined', inject([CartService], (service: CartService) => {
+    let data:any[] = [17.9,15.86,20.99];
+    let total:number;
+    let vat:number = 20;
+    expect(service.doTotal(data, total, vat)).toBeDefined();
+  }));
+
+  it('should calcVat() to be defined', inject([CartService], (service: CartService) => {
+    let value:number;
+    let subTotal:number = 143;
+    let vat:number = 20;
+    expect(service.calcVat(value, subTotal, vat)).toBeTruthy();
+    expect(service.calcVat(value, subTotal, vat)).toBeDefined();
+  }));
+
+  it('should calcVat() return a number', inject([CartService], (service: CartService) => {
+    let value:number;
+    let subTotal:number = 143;
+    let vat:number = 20;
+    value = service.calcVat(value, subTotal, vat);
+    expect(value).toBeTruthy();
+    expect(value).toBeDefined();
+    expect(value).not.toBeNull();
+    expect(value).toEqual(jasmine.any(Number))
+  }));
+
+  it('should calcVat() returned value to equal 28.6', inject([CartService], (service: CartService) => {
+    let value:number;
+    let subTotal:number = 143;
+    let vat:number = 20;
+    value = service.calcVat(value, subTotal, vat);
+    expect(value).not.toBeNull();
+    expect(value).toEqual(jasmine.any(Number));
+    expect(value).toEqual(28.6);
+  }));
+
 });
--- src/app/shared/services/cart.service.ts
+++ src/app/shared/services/cart.service.ts
@@ -6,4 +6,23 @@ import { Injectable } from '@angular/core';
 export class CartService {
 
   constructor() { }
+
+  doTotal(data:any[], total:number, vat:number):number{
+    
+    const reducer = (a:number, b:number) => a + b;
+    const priceArray = data.map(elem =>elem.price );
+    console.log("Price array :", priceArray);
+    let subTotal = priceArray.reduce(reducer);
+    let returnedVal:number;
+    let vatVal = this.calcVat(returnedVal, subTotal, vat);
+    return total = subTotal + vatVal;
+
+  }
+
+  calcVat(value:number, subTotal:number, vat:number):number{
+
+    return value = Number(((subTotal/100)*vat).toFixed(2));
+
+  }
+
 }
